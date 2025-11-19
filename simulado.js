6eae09484c7a9a96166f018c0f066198228244ae
Gilmar Rodrigues Campelo <gilmarcampelo@gmail.com>
2025-11-18 23:44:24 -0300

    Simulado01_Ajuste

--- simulado.js
+++
@@ -1,7 +0,0 @@
-// 1. Importe o módulo e atribua-o a uma variável (usando o require)
-//const prompt = require('prompt-sync')();
-const prompt = require('prompt-sync')();
-const primeiroNome = prompt("Digite seu primeiro nome: ");
-console.log(`Olá, ${primeiroNome}! Bem-vindo ao sistema de análise de repasses financeiros.`);
-console.log("--------------------------------------------------");
-
